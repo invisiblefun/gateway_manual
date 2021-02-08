e02708d3c983b772ba1ea5ea358779d1133951be
Ewen yau <ewenyau@Ewen-Yau-MacBook-pro-Sponsored-by-Zap.local>
2021-02-08 12:24:34 +0800

    update gateway information

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,16 @@
+// import { css } from 'docz-plugin-css'
+
 export default {
   title: "InvisibleFun Studio Limited",
   base: "/gateway_manual/",
   ignore: [
     "README.md"
-  ]
+  ],
+  indexHtml: './index.html'
+  // plugins: [
+  //   css({
+  //     preprocessor: 'sass',
+  //     cssmodules: true
+  //   })
+  // ]
 }
